9952009708ea20986c8ee3880b6c182928e5fbb1
Gabriel <86237777+Gabriel-M-N-da-Silva@users.noreply.github.com>
2021-09-14 12:24:38 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,21 +1,26 @@
-function Converter() {
-  var moedaDe = document.getElementById("de").value;
-  var moedaPara = document.getElementById("para").value;
+/*
+Conversor de moedas Dólar ---> Real
+*/
 
-  var elementoInput = document.getElementById("valor");
-  var valorDoInput = parseFloat(elementoInput.value);
-  var simbolo;
+function Converter() {
+  var moedaDe = document.getElementById("de").value; // De qual moeda vai ser convertida - "Real"/"Dólar"
+  var moedaPara = document.getElementById("para").value; // Para qual moeda vai ser convertida - "Real"/"Dólar
+  var elementoInput = document.getElementById("valor"); //Elemento da caixa para escrever
+  var valorDoInput = parseFloat(elementoInput.value); //Oque está escrito na caixa
+  var simbolo; // R$ ou U$
   var valorConvertido;
 
-  if (moedaDe == "Dólar" && moedaPara == "Real") {
+  
+  // Conversão
+  if (moedaDe == "Dólar" && moedaPara == "Real") { //Dólar para real
     valorConvertido = (valorDoInput * 5.43).toFixed(2);
     simbolo = "R$ ";
   }
-  if (moedaDe == "Real" && moedaPara == "Dólar") {
+  if (moedaDe == "Real" && moedaPara == "Dólar") { //Real pra dólar
     valorConvertido = (valorDoInput / 5.43).toFixed(2);
     simbolo = "U$ ";
   }
-  if (moedaDe == moedaPara) {
+  if (moedaDe == moedaPara) { //Mesma moeda
     if (moedaDe == "Real") {
       simbolo = "R$";
     } else {
@@ -23,10 +28,12 @@ function Converter() {
     }
     valorConvertido = valorDoInput.toFixed(2);
   }
+  
+  // Print do resultado
+  
+  var elementoConvertido = document.getElementById("valorConvertido"); //Elemento que vai aparecer o resultado
 
-  var elementoConvertido = document.getElementById("valorConvertido");
-
-  if (elementoInput.value != "") {
+  if (elementoInput.value != "") { //Se não estiver vazio
     elementoConvertido.innerHTML = simbolo + valorConvertido;
   }
 }
