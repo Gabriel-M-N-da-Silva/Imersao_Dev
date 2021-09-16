67f96a111337b7634afa8eec1cedd14b37d1fc3b
Gabriel <86237777+Gabriel-M-N-da-Silva@users.noreply.github.com>
2021-09-16 10:53:54 -0300

    Arquivos aula 4
    Adicionado arquivos da aula 4 (Oficial).
    Projeto: Aluraflix;

---
+++ Principal/Aula 4/Aluraflix/dist/script.js
@@ -0,0 +1,10 @@
+var logoFilmes = [
+  "https://br.web.img3.acsta.net/medias/nmedia/18/95/59/60/20417256.jpg",
+  "https://br.web.img3.acsta.net/pictures/19/12/18/16/25/0588210.jpg",
+  "https://br.web.img2.acsta.net/pictures/18/03/16/15/08/2019826.jpg"
+];
+
+
+for (var i = 0; i < logoFilmes.length; i++) {
+  document.write("<img src=" + logoFilmes[i] + ">");
+}
